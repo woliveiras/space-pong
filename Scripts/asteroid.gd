1d6f86afb5c2f4e6a433b177a4dc8c354c0d5857
William Oliveira <w.oliveira542@gmail.com>
2025-04-11 21:46:02 +0200

    feat: add layers

--- Scripts/asteroid.gd
+++ Scripts/asteroid.gd
@@ -24,3 +24,8 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 
 func _on_timer_delete_timeout() -> void:
 	queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+	body.pongs += 1
+	queue_free()
+	
