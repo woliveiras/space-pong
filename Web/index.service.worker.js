d2bcbfea6a976d58615715606ff0261f7227eeed
William Oliveira <w.oliveira542@gmail.com>
2025-04-12 10:37:41 +0200

    release web

--- Web/index.service.worker.js
+++ Web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1744444827|12389765086';
+const CACHE_VERSION = '1744447021|14583473411';
 /** @type {string} */
 const CACHE_PREFIX = 'Space Pong-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
